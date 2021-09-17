3753462f825e7357c6c7a7a0835bafe39604534f
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-09-17 18:25:06 -0500

    Update User class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+import classes.User;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String username;
+        String password;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter username: ");
+        username = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        password = scanner.nextLine();
+        
+
     }
 }
--- src/classes/User.java
+++ src/classes/User.java
@@ -2,21 +2,42 @@ package classes;
 
 public class User {
 
-    private String name;
+    private String username;
     private String password;
 
-    private User(String name, String password) {
-        this.name = name;
+    private User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
     static private User _instance = null;
 
-    static public User instance(String name, String password) {
+    static public User instance(String username, String password) {
         if (_instance == null) {
-            _instance = new User(name, password);
+            _instance = new User(username, password);
         }
         return _instance;
+    }
+
+    @Override
+    public String toString() {
+        return "User [username=" + username + ", password=" + password + "]";
     }
 
 }
